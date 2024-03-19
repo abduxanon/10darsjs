34bf45829ae700281388d16115bb460db6eb871c
abduxanon <abduxanonaxmadaliyev@gmail.com>
2024-03-20 04:34:27 +0500

    dars

---
+++ app.js
@@ -0,0 +1,13 @@
+class User {
+  constructor(ism, yosh,mamlakat) {
+    this.ism = ism;
+    this.yosh = yosh;
+    this.mamlakat = mamlakat;
+
+  }
+}
+let user1 = new User("abduxanon",17, "uzbekistan");
+let user2 = new User("abdulox",17, "uzbekistan");
+
+console.log(user1);
+console.log(user2);
